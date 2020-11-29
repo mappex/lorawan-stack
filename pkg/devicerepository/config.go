64a1649c21411ffabe90b5b957d96a97d2bdc0cf
Aggelos Kolaitis <aggelos.kolaitis@thethingsindustries.com>
2020-11-29 19:38:19 +0200

    dr: Introduce bleveStore

--- pkg/devicerepository/config.go
+++ pkg/devicerepository/config.go
@@ -18,7 +18,16 @@ import (
 	"context"
 
 	"go.thethings.network/lorawan-stack/v3/pkg/devicerepository/store"
+	"go.thethings.network/lorawan-stack/v3/pkg/devicerepository/store/bleve"
+	"go.thethings.network/lorawan-stack/v3/pkg/errors"
+	"go.thethings.network/lorawan-stack/v3/pkg/fetch"
 )
+
+// IndexConfig represents configuration for the DeviceRepository index files.
+type IndexConfig struct {
+	Type  string       `name:"type" description:"Service to use for indexing (bleve)"`
+	Bleve bleve.Config `name:"bleve"`
+}
 
 // Config represents the DeviceRepository configuration.
 type Config struct {
@@ -30,12 +39,46 @@ type Config struct {
 
 	AssetsBaseURL string `name:"assets-base-url" description:"The base URL for assets"`
 	RequireAuth   bool   `name:"require-auth" description:"Require authentication for the device repository"`
+
+	Index IndexConfig `name:"index"`
 }
+
+var (
+	errUnknownIndexType = errors.DefineUnimplemented("unknown_index_type", "unknown index type `{type}`")
+)
 
 // NewStore creates a new Store for end devices.
 func (c Config) NewStore(ctx context.Context) (store.Store, error) {
 	if c.Store != nil {
 		return c.Store, nil
 	}
-	return &store.NoopStore{}, nil
+	var fetcher fetch.Interface
+	switch {
+	case c.Static != nil:
+		fetcher = fetch.NewMemFetcher(c.Static)
+	case c.Directory != "":
+		fetcher = fetch.FromFilesystem(c.Directory)
+	case c.URL != "":
+		var err error
+		fetcher, err = fetch.FromHTTP(c.URL, true)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return &store.NoopStore{}, nil
+	}
+	s, err := store.NewRemoteStore(fetcher)
+	if err != nil {
+		return nil, err
+	}
+	// Wrap with indexed store if configureds
+	switch indexConfig := c.Index; indexConfig.Type {
+	case "":
+		return s, err
+	case "bleve":
+		indexConfig.Bleve.Store = s
+		return indexConfig.Bleve.NewStore(ctx)
+	default:
+		return nil, errUnknownIndexType.WithAttributes("type", indexConfig.Type)
+	}
 }
