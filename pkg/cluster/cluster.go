f5772a832931946b45fa3a42aa74e3447bcc3a5c
Aggelos Kolaitis <aggelos.kolaitis@thethingsindustries.com>
2020-12-10 20:45:25 +0200

    all: Add device repository cluster peer

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -149,6 +149,7 @@ func defaultNew(ctx context.Context, config *Config, options ...Option) (Cluster
 	c.addPeer("as", config.ApplicationServer, ttnpb.ClusterRole_APPLICATION_SERVER)
 	c.addPeer("js", config.JoinServer, ttnpb.ClusterRole_JOIN_SERVER)
 	c.addPeer("cs", config.CryptoServer, ttnpb.ClusterRole_CRYPTO_SERVER)
+	c.addPeer("dr", config.DeviceRepository, ttnpb.ClusterRole_DEVICE_REPOSITORY)
 
 	for _, join := range config.Join {
 		c.peers[join] = &peer{
